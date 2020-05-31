d1e5a3388065db11b59dfc168cac4e65cd73365a
unknown <pemeraldy@gmail.com>
2020-05-31 01:30:55 +0100

    added crud functionalities

--- python_course.py
+++ python_course.py
@@ -11,6 +11,37 @@ class DatabaseManage(object):
                 cur.execute("CREATE TABLE IF NOT AVAILABLE course(Id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, price TEXT, is_private BOOLEAN NOT NULL DEFAULT 1)")
         except Exception:
             print("Bad Connection")
+    
+    #TODO: create data
+    def insert_data(self, data):
+        try:
+            with con:
+                cur = con.cursor()
+                cur.execute(
+                    "INSERT INTO course(name, description, price, is_private) VALUES (?,?,?,?)", data
+                )
+        except Exception:
+            return False
 
+    #TODO: read data
+    def fetch_data(self):
+        try:
+            with con:
+                cur = con.cursor
+                cur.execute("SELECT * FROM course")
+                return cur.fetchall()
+        except Exception:
+            return False
+            
+
+    #TODO: delete data
+    def delete_data(self, id):
+        try:
+            with con:
+                cur = con.cursor()
+                sql = "DELETE FROM course WHERE id = ?"
+                cur.execute(sql, [id])
+        except Exception:
+            return False 
 
 # TODO: Provide UI to users
